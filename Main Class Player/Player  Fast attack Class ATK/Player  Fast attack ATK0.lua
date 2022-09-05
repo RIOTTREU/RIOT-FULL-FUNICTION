92c5ce335d566ed56f7620b43ec59c44f292f0f6
RIOT <62182170+RIOTTREU@users.noreply.github.com>
2022-09-05 22:06:44 +0700

    Add files via upload

---
+++ Main Class Player/Player  Fast attack Class ATK/Player  Fast attack ATK0.lua
@@ -0,0 +1,35 @@
+gg.clearResults()
+function RIOT(address,flags,freeze,name,value)
+    RTOL = {}
+    RTOL[1] = {}
+    RTOL[1].address = address
+    RTOL[1].flags = flags
+    RTOL[1].value = value
+    RTOL[1].freeze = freeze
+    RTOL[1].name = name
+    gg.setValues(RTOL)
+    gg.addListItems(RTOL)
+    gg.clearResults(RTOL)
+end
+function RTM(address,flags,value,freeze,freezeFrom,freezeTo)
+    p = {}
+	p[1] = {}
+	p[1].address = address
+	p[1].flags = flags
+	p[1].value = value
+	p[1].freeze = freeze
+	p[1].freezeType = gg.FREEZE_IN_RANGE
+	p[1].freezeFrom = freezeFrom
+	p[1].freezeTo = freezeTo
+	gg.setValues(p)
+	gg.addListItems(p)
+    gg.clearResults(p)
+end
+gg.clearResults()
+gg.searchNumber('3;1;-1;0~9999::13', gg.TYPE_DWORD)
+gg.refineNumber('3', gg.TYPE_DWORD)
+Rt = gg.getResults(1)
+function ATK0()
+    RIOT(Rt[1].address + 0x8C,4,true,"Attack",0)
+    RIOT(Rt[1].address + 0xA8,4,true,"Attack",0)
+end
